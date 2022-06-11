ac215a26f0a2ae729481d132fb08a7e2681ffccd
AalaaDabbah <95836015+AalaaDabbah@users.noreply.github.com>
2022-06-11 15:02:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -35,6 +35,6 @@ def nothing(name:str="alaa"):
 def nothing(id):
     return {"message":id}
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1",port=7000)
+#if __name__ == "__main__":
+ #   uvicorn.run(app, host="127.0.0.1",port=7000)
 
